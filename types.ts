76806e49f513475dfe3c8368af75a9de42b5ec20
Kalmovic <joseph.ext@rallyrd.com>
2023-08-28 01:47:58 -0300

    task: types file

---
+++ types.ts
@@ -0,0 +1,39 @@
+export type BillboardType = {
+  id: string;
+  label: string;
+  imageUrl: string;
+};
+
+export type CategoryType = {
+  id: string;
+  name: string;
+  imageUrl: string;
+};
+
+export type ProductType = {
+  id: string;
+  name: string;
+  category: CategoryType;
+  price: string;
+  isFeatured: boolean;
+  size: SizeType;
+  color: ColorType;
+  images: Image[];
+};
+
+export type SizeType = {
+  id: string;
+  name: string;
+  value: string;
+};
+
+export type ColorType = {
+  id: string;
+  name: string;
+  value: string;
+};
+
+export type Image = {
+  id: string;
+  url: string;
+};
