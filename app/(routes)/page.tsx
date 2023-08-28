1bde0217667d762204b405eb69f9b48865b68bd7
Kalmovic <joseph.ext@rallyrd.com>
2023-08-28 01:48:31 -0300

    feat: main page

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import { getBillboard } from "@/actions/get-billboard";
+import { getProducts } from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+import React from "react";
+
+export const revalidate = 0;
+
+async function HomePage() {
+  const products = await getProducts({
+    isFeatured: "true",
+  });
+  const billboard = await getBillboard("9c5f5e43-1897-4b01-9d99-298eeea746b7");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" products={products} />
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default HomePage;
