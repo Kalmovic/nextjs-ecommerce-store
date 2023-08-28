bb3ffc7468017dcc4791acb941a6a6182614bb77
Kalmovic <joseph.ext@rallyrd.com>
2023-08-28 02:14:49 -0300

    feat: add redirect to product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import { ProductType } from "@/types";
 import Image from "next/image";
 import React from "react";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 type ProductCardProps = {
   product: ProductType;
 };
 
 function ProductCard({ product }: ProductCardProps) {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/product/${product.id}`);
+  };
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       {/* imgs and actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
