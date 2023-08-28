e28c64c20a229060d00314866c342e524208b78e
Kalmovic <joseph.ext@rallyrd.com>
2023-08-28 02:50:12 -0300

    feat: more actions

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -1,6 +1,6 @@
 import { ProductType } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/product`;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
 export const getProduct = async (id: string): Promise<ProductType> => {
   const response = await fetch(`${URL}/${id}`);
