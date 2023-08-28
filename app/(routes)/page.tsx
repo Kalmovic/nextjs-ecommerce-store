3046a36e34a6d784fe46b6722554dc19abd10513
Kalmovic <joseph.ext@rallyrd.com>
2023-08-28 20:38:46 -0300

    fix: better implement home billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,7 +4,6 @@ import Billboard from "@/components/ui/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 import React from "react";
-import { getBillboards } from "@/actions/get-billboards";
 
 export const revalidate = 0;
 
@@ -12,11 +11,13 @@ async function HomePage() {
   const products = await getProducts({
     isFeatured: "true",
   });
-  const billboards = await getBillboards();
+  // Fixed billboard id that we want to display on the homepage
+  const billboard = await getBillboard("0e37f161-f7f4-42b7-b1a6-3dac65e74e3b");
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboards[0]} />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" products={products} />
         </div>
