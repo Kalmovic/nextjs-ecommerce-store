7116ae9a0442978524fccfc165d2681669808795
Kalmovic <joseph.ext@rallyrd.com>
2023-08-28 22:39:34 -0300

    rm border from billboard label

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -15,7 +15,7 @@ function Billboard({ data }: BillboardProps) {
         className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8 ">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs backdrop-blur-sm rounded-sm border-red-600/20 border-2 bg-opacity-10 p-2">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs backdrop-blur-sm rounded-sm bg-opacity-10 p-2">
             {data.label}
           </div>
         </div>
